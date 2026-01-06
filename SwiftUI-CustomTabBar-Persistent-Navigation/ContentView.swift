8b89047fc43c2beac3406b2bc5df55e8efcfdc39
Abdul Azeem <abdul.tech689@gmail.com>
2026-01-06 17:28:51 +0530

    Initial Commit

---
+++ SwiftUI-CustomTabBar-Persistent-Navigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-CustomTabBar-Persistent-Navigation
+//
+//  Created by Abdul Azeem on 06/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-CustomTabBar-Persistent-Navigation/SwiftUI_CustomTabBar_Persistent_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CustomTabBar_Persistent_NavigationApp.swift
+//  SwiftUI-CustomTabBar-Persistent-Navigation
+//
+//  Created by Abdul Azeem on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CustomTabBar_Persistent_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
