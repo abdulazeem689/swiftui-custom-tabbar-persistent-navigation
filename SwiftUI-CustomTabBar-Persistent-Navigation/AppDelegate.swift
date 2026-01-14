33741c973b77e8826f07e5afd1ea8c4bc47a8135
Abdul Azeem <abdul.tech689@gmail.com>
2026-01-14 22:55:43 +0530

    adding app and scene delegate.

---
+++ SwiftUI-CustomTabBar-Persistent-Navigation/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SwiftUI-CustomTabBar-Persistent-Navigation
+//
+//  Created by Abdul Azeem on 14/01/26.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?)
+    -> Bool
+    {
+        return true
+    }
+}
---
+++ SwiftUI-CustomTabBar-Persistent-Navigation/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  SwiftUI-CustomTabBar-Persistent-Navigation
+//
+//  Created by Abdul Azeem on 14/01/26.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions)
+    {
+        
+    }
+}
